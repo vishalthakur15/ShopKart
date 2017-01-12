9b48544ff3cde5670fafe1b06d854e4418b81d9b
Vishal Thakur <Vishal_Thakur@epam.com>
2017-01-12 21:04:29 +0530

    added mongo changes in appp.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // connect to MongoDB
-mongoose.connect('mongodb://userO24:uAuDuR0XlmX4Xwjw@mongodb/sampledb')
+mongoose.connect('mongodb://userGWE:deuXyUph6qCDgedS@mongodb/sampledb')
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
